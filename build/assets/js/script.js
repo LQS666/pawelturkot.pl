11993176b494c859632a364fd77ee57b40d0b2c2
Paweł Turkot <p.turkot123@gmail.com>
2019-02-14 23:46:08 +0100

    Add quote

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,11 +1,57 @@
-mcroyal = () => {
+(function () {
+    // Hamburger menu
     const mcroyal = document.querySelector('.mcroyal'),
-        nav = document.querySelector('nav');
+        nav = document.querySelector('nav'),
+        headerLinks = $('nav li a');
 
-    mcroyal.addEventListener('click', function () {
-        mcroyal.classList.toggle('active');
-        nav.classList.toggle('active');
+    const openHamburgerMenu = () => {
+        mcroyal.addEventListener('click', function () {
+            mcroyal.classList.toggle('active');
+            nav.classList.toggle('active');
+            noscroll();
+        });
+    };
+    openHamburgerMenu();
+
+    // Block scroll after open menu
+
+    function noscroll() {
+        window.scrollTo(0, 0);
+    }
+
+    window.addEventListener('scroll', function () {
+        if (mcroyal.classList.contains('active')) {
+            noscroll();
+        } else {
+            window.removeEventListener('scroll', noscroll);
+        }
     });
-}
 
-mcroyal();
+
+    // Scroll to hash and close menu
+
+    function closemenu() {
+        mcroyal.classList.remove('active');
+        nav.classList.remove('active');
+    }
+
+    $('nav a').on('click', function () {
+        const goToSection = "[data-section=" + $(this).attr('class') + "]";
+        closemenu();
+        $('body, html').animate({
+            scrollTop: $(goToSection).offset().top
+        })
+    })
+
+
+    // Preloader 
+
+    const preloader = document.querySelector('.preloader');
+
+    preloader.classList.add('visible');
+    window.addEventListener('load', function () {
+        setTimeout(function () {
+            preloader.classList.remove('visible');
+        }, 3000);
+    });
+}());
