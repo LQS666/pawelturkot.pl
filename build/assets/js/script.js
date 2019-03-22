08007a7875c64664272d64444c77fab6235becd2
Paweł Turkot <p.turkot123@gmail.com>
2019-03-22 01:45:39 +0100

    Fix scrolling header arrow

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -43,6 +43,15 @@
         })
     })
 
+    // Scroll to hash
+
+    $('header a').on('click', function () {
+        const goToSection = "[data-section=" + $(this).attr('class') + "]";
+        $('body, html').animate({
+            scrollTop: $(goToSection).offset().top
+        })
+    })
+
 
     // Preloader 
 
